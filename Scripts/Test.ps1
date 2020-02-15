d0d88254fae2965eb24110ce1848a2c7fc1c56fd
Vivek RR <60930583+vivek7rr@users.noreply.github.com>
2020-02-15 23:22:03 +0000

    Update Test.ps1

--- Scripts/Test.ps1
+++ Scripts/Test.ps1
@@ -1,18 +1,28 @@
 Function Pingmachine
-{
-    Param($Machines)
-    foreach ($Machine in $Machines)
-    {
-        if(!(Test-Connection -Cn $Machine -BufferSize 16 -Count 1 -ea 0 -quiet))
-    {
-        $wpf.output.AppendText("$Machine : Offline`n")
-    }
-        else
-        {
+            {
+                $wpf.outbox = $Null
+                $wpf.Outbox.AppendText("Please wait...`n`n")
+                Param($Machines)
+                foreach ($Machine in $Machines)              
+                {
+                $wpf.Outbox.Focus()
 
-        $wpf.output.AppendText("$Machine : Online`n")
 
-        }
+                    if(!(Test-Connection -Cn $Machine -BufferSize 16 -Count 1 -ea 0 -quiet))
+                    {
 
-    }
-}
+                        $wpf.Outbox.AppendText("$Machine : Offline`r`n")
+   
+                    }
+                    
+                    else
+                    {
+
+                    $wpf.Outbox.AppendText("$Machine : Online`r`n")
+
+                    }
+                [void] [System.Windows.Forms.Application]::DoEvents()
+                }
+                $wpf.Outbox.AppendText("Ping Action Completed")
+                
+            }
