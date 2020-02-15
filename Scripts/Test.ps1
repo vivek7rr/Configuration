98e2bb860fa88bf18fa0547986055e4955a05c87
Vivek RR <60930583+vivek7rr@users.noreply.github.com>
2020-02-15 19:35:26 +0000

    Update Test.ps1

--- Scripts/Test.ps1
+++ Scripts/Test.ps1
@@ -4,3 +4,21 @@ function testfuntion
  Get-Service -ComputerName $machine
 
 }
+Function Pingmachine
+{
+    Param($Machines)
+    foreach ($Machine in $Machines)
+    {
+        if(!(Test-Connection -Cn $Machine -BufferSize 16 -Count 1 -ea 0 -quiet))
+    {
+        $wpf.output.Text = "$Machine : Offline"
+    }
+        else
+        {
+
+        $wpf.output.Text = "$Machine : Online"
+
+        }
+
+    }
+}
